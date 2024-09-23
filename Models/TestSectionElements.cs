7d17bf23a836e5b3e9c404d1fbb8998a65230b9e
b-emr <befe22@ku.edu.tr>
2024-09-23 14:11:26 +0300

    secondCOmmit

---
+++ Models/TestSectionElements.cs
@@ -0,0 +1,10 @@
+namespace erentitan.Models;
+
+public class TestSectionElements
+{
+    public long Id { get; set; }
+    public string ImageUrl { get; set; }
+    public int ImageWidth { get; set; }
+    public int ImageHeight { get; set; }
+    public string Xaml { get; set; }
+}
